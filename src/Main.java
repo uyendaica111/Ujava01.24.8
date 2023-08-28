7e769dd21ce185abf3c1a3002904a2f5b93acb3b
uyendaica111 <chuong14041971@gmail.com>
2023-08-29 00:00:36 +0700

    demo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // c1:
+        System.out.println("Uyendaica111");
+
+        // c2:
+        System.out.print("Uyen");
+        System.out.print("Uyen");
+
+        //c3:
+        System.out.printf("Uyen");
+    }
+}
